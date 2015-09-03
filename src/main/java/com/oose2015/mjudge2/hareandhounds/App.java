252a6c37fbc71cac97ca7f25d44efd5c7e1e94d3
mjudge2 <mjudge2@jhu.edu>
2015-09-02 21:31:53 -0400

    initial hare and hound oose project commit

---
+++ src/main/java/com/oose2015/mjudge2/hareandhounds/App.java
@@ -0,0 +1,13 @@
+package com.oose2015.mjudge2.hareandhounds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
